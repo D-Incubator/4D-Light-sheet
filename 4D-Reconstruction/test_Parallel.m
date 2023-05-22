0a0abd27b1267eb363c4234736bce6c81b60a261
d-incubator1 <128861688+d-incubator1@users.noreply.github.com>
2023-05-22 07:40:24 -0500

    Update test_Parallel.m

--- 4D-Reconstruction/test_Parallel.m
+++ 4D-Reconstruction/test_Parallel.m
@@ -1,3 +1,43 @@
+% Instruction: This code serves as the 4D reconstruction algorithm for
+% zebrafish heart. The idea of this method originates from the paper: X.
+% Zhang et al., "4D Light-Sheet Imaging and Interactive Analysis of 
+% Cardiac Contractility in Zebrafish Larvae".It combines parallel computing
+% with retrospective synchronization for the four-dimensional (4D) 
+% reconstruction of quasi-periodic cardiac dynamics.
+%
+%
+% Disclaimer: This code was initiated by Xinyuan Zhang on 
+% 7/5/2022. For any information, please reach out to Xinyuan at 
+% xinyuan.zhang@utdallas.edu or Yichen at yichen.ding@utdallas.edu.
+%
+% Copyright (c) 2023, Xinyuan Zhang, Yichen Ding
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+%
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the
+%       distribution
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+% IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+% CONTRIBUTOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
 clear;
 
 %% Set before running the code
